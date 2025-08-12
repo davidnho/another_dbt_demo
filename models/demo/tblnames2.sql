e2f7dc241c2dda8504e29f27e197eaa2690a331c
NOEL DAVID <noelsisondavid@gmail.com>
2025-08-12 06:09:41 +0000

    Deleted some models

--- models/demo/tblnames2.sql
+++
@@ -1,6 +0,0 @@
-
-{{ config(materialized='table') }}
-
-select *
-from {{ ref('tblnames1') }}
-where firstname = 'Noel'
--- models/demo/view_age_less_than_30.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ ref('tblnames1') }}
-where age < 30
